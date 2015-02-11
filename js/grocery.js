35f66f5ff306818d69c05c6673ffae3cb514ed0f
William Tsang & Leonard Bui <pair+kaawang.lnbcoding@devbootcamp.com>
2015-02-11 12:16:07 -0800

    Total working

--- js/grocery.js
+++ js/grocery.js
@@ -24,7 +24,8 @@
         $scope.droppedObjects2= [];
         $scope.onDropComplete1=function(data,evt){
             var index = $scope.droppedObjects1.indexOf(data);
-            if (index == -1)
+            console.log(index);
+            // if (index == -1) // has to be unique
             $scope.droppedObjects1.push(data);
         }
         $scope.onDragSuccess1=function(data,evt){
@@ -34,19 +35,12 @@
                 $scope.droppedObjects1.splice(index, 1);
             }
         }
-        $scope.onDragSuccess2=function(data,evt){
-            var index = $scope.droppedObjects2.indexOf(data);
-            if (index > -1) {
-                $scope.droppedObjects2.splice(index, 1);
-            }
-        }
-        $scope.onDropComplete2=function(data,evt){
-            var index = $scope.droppedObjects2.indexOf(data);
-            if (index == -1) {
-                $scope.droppedObjects2.push(data);
-            }
-        }
-        var inArray = function(array, obj) {
-            var index = array.indexOf(obj);
+        $scope.getTotal = function(){
+          var total = 0;
+          for(var i = 0; i < $scope.droppedObjects1.length; i++){
+            var itemPrice = $scope.droppedObjects1[i].price;
+            total += itemPrice;
+          }
+          return total;
         }
       });
